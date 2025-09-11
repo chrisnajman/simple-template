78f51a47beb2994973e4c9544283bb05cc4f0467
Chris Najman <christopher.najman@gmail.com>
2025-09-11 08:15:31 +0100

    Deleted redundant primary-nav-links.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import themeSwitcher from "./js-modules/theme.js"
 import loadingAnimation from "./js-modules/loader.js"
-import btnHamburger from "./js-modules/primary-navigation/hamburger-button.js"
-import primaryNavLinks from "./js-modules/primary-navigation/primary-nav-links.js"
+import btnHamburger from "./js-modules/hamburger-button.js"
 import modulePlaceholder from "./js-modules/module-placeholder.js"
 
 themeSwitcher()
 loadingAnimation()
 btnHamburger()
-primaryNavLinks()
 modulePlaceholder()
